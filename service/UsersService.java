fd8ba39f427765a46b17f6a71df9dfa46212c12e
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-19 13:19:12 -0600

    Feature | added CRUD operation for Users Service

---
+++ service/UsersService.java
@@ -0,0 +1,147 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import database.DatabaseCredentials;
+import model.Users;
+
+public class UsersService {
+
+    /*
+    This method will return Users object with the given id.
+    */
+    public Users get(int id) {
+        Users users = new Users();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DatabaseCredentials.JDBC_URL,
+                    DatabaseCredentials.DATABASE_USER,
+                    DatabaseCredentials.DATABASE_PASS);
+
+            if(con != null){
+                String selectQuery = "SELECT * from users where id=? ;";
+                PreparedStatement pstmt = con.prepareStatement(selectQuery);
+                pstmt.setInt(0, id);
+                ResultSet rs = pstmt.executeQuery();
+                while(rs.next()){
+                    String name = rs.getString("name");
+                    String email = rs.getString("email");
+                    LocalDate joinedDate = rs.getDate("joined_date").toLocalDate();
+
+                    users.setId(id);
+                    users.setName(name);
+                    users.setEmail(email);
+                    users.setJoinedDate(joinedDate);
+                }
+            }
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    /*
+    This method will return list of Users.
+    */
+    public List<Users> getAll() {
+        List<Users> usersList = new ArrayList<>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DatabaseCredentials.JDBC_URL,
+                    DatabaseCredentials.DATABASE_USER,
+                    DatabaseCredentials.DATABASE_PASS);
+
+            if(con != null){
+                String selectQuery = "SELECT * from users;";
+                PreparedStatement pstmt = con.prepareStatement(selectQuery);
+                ResultSet rs = pstmt.executeQuery();
+
+                while(rs.next()){
+                    int id = rs.getInt("id");
+                    String name = rs.getString("name");
+                    String email = rs.getString("email");
+                    LocalDate joinedDate = rs.getDate("joined_date").toLocalDate();
+
+                    Users users = new Users();
+                    users.setId(id);
+                    users.setName(name);
+                    users.setEmail(email);
+                    users.setJoinedDate(joinedDate);
+                    usersList.add(users);
+                }
+            }
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return usersList;
+    }
+
+    /*
+    This method will create a user.
+    */
+    public void create(Users users) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DatabaseCredentials.JDBC_URL,
+                    DatabaseCredentials.DATABASE_USER,
+                    DatabaseCredentials.DATABASE_PASS);
+
+            if(con != null){
+                String insertQuery = "INSERT INTO users (name, email, joined_date) values(?, ?, ?);";
+                PreparedStatement pstmt = con.prepareStatement(insertQuery);
+                pstmt.setString(0, users.getName());
+                pstmt.setString(1, users.getEmail());
+                pstmt.setDate(2, java.sql.Date.valueOf(users.getJoinedDate()));
+                if(pstmt.executeUpdate()>0){
+                    System.out.println("Data inserted successfully");
+                }else{
+                    System.out.println("Data insertion failed");
+                }
+            }
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+    This method will update user.
+    */
+    public void update(Users users) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DatabaseCredentials.JDBC_URL,
+                    DatabaseCredentials.DATABASE_USER,
+                    DatabaseCredentials.DATABASE_PASS);
+
+            if(con != null){
+                String updateQuery = "UPDATE users set name=?, email=?, joined_date=? where id=? ;";
+                PreparedStatement pstmt = con.prepareStatement(updateQuery);
+                pstmt.setString(0, users.getName());
+                pstmt.setString(1, users.getEmail());
+                pstmt.setDate(2, java.sql.Date.valueOf(users.getJoinedDate()));
+                pstmt.setInt(3, users.getId());
+
+                if(pstmt.executeUpdate() > 0){
+                    System.out.println("Data Updated successfully");
+                }else{
+                    System.out.println("Data Update failed");
+                }
+            }
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
