b3386d4d8d83258b5191159d664112ec24522df7
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-21 16:25:47 -0600

    Update

--- database/DatabaseCredentials.java
+++ database/DatabaseCredentials.java
@@ -5,7 +5,8 @@ public class DatabaseCredentials {
     public static final int DATABASE_PORT = 3306;
     public static final String DATABASE_HOST = "localhost";
     public static final String DATABASE_USER = "test";
-    public static final String DATABASE_PASS = "Test@123";
+    // public static final String DATABASE_PASS = "Test@123";
+    public static final String DATABASE_PASS1 = "Test@123#";
 
     public static final String JDBC_URL = "jdbc:mysql://" + DATABASE_HOST + ":" + DATABASE_PORT + "/" + DATABASE_NAME;
 
